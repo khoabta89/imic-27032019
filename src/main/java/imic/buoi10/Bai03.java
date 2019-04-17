ef398eb03ea07557bec4636fa694ce160afca9a5
khoabta89 <khoabta.sgtl@gmail.com>
2019-04-17 20:38:57 +0700

    Buoi 10: Bai 03 Refix

---
+++ src/main/java/imic/buoi10/Bai03.java
@@ -0,0 +1,24 @@
+package imic.buoi10;
+
+import static java.lang.StrictMath.sqrt;
+
+public class Bai03
+{
+    public static boolean kiemTraSoNguyenTo (int n) {
+        if (n < 2)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
