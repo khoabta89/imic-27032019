a61747eea540e380a729e824298f573e52eb970a
khoabta89 <khoabta.sgtl@gmail.com>
2019-04-19 20:14:22 +0700

    Buoi 12: Bai 01 - tinhCanBacHai

--- src/main/java/imic/Buoi12/Bai01.java
+++ src/main/java/imic/Buoi12/Bai01.java
@@ -1,7 +1,8 @@
 package imic.Buoi12;
 
-public class Bai01 {
-    public static double doNumberAndArray() {
+public class Bai01
+{
+    public static double inSoNhoNhat() {
         double[] myList = {-3, 7, -1.5, 3.7, 10, 7, 0, 7, -1.5};
         double min = myList[0];
         for (int i = 1; i < myList.length; i++) {
@@ -12,8 +13,18 @@ public class Bai01 {
         return min;
     }
 
-    public static void doCanBacHai()
+    public static double tinhCanBacHai()
     {
-        double canBacHai = 0;
+        double[] myList = {-3, 7, -1.5, 3.7, 10, 7, 0, 7, -1.5};
+        double ketQua = Math.sqrt(myList[0]);
+        for (int i = 1; i < myList.length; i++)
+        {
+            if (myList[i] > 0)
+            {
+                ketQua = Math.sqrt(myList[i]);
+                System.out.println("Can ban 2 cac so: " +String.format("%.2f",ketQua) +",");
+            }
+        }
+        return ketQua;
     }
 }
